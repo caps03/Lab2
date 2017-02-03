43da9954c28f4feb0c7de941e7be4b668596038a
Steven Cap <caps01@udayton.edu>
2017-02-03 18:38:47 -0500

    created part three

--- Lab2/Lab2/Lab2.c
+++ Lab2/Lab2/Lab2.c
@@ -14,10 +14,19 @@ main()
 	char *heap = malloc(13);//this is on the heap
 	heap = "ABCDEFGHIJKL";
 	char *data = heap;//in the data section
+
+	//Part Three
+	char* sth = malloc(14);//this is on the heap
+	sth = func_two();
 }
 func_one(char * j)//this is for part one bss to stack
 {
 	printf("Enter a 12 letter word:\n");
 	scanf_s("%s", j);//this will be on the stack
 	return 0;
+}
+func_two()
+{
+	char *sth = "ABCDEFGHIJKLM";//this is on the stack
+	return sth;
 }
