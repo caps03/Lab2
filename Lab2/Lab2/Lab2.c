c22dfd7f9e412ea9b3bed84b3c38d7ab24f5fc30
Steven Cap <caps01@udayton.edu>
2017-02-03 18:31:23 -0500

    created part one

--- Lab2/Lab2/Lab2.c
+++ Lab2/Lab2/Lab2.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#pragma bss_seg(".myBSS")  
+char* j;                     // stored in "my_data1" 
+
 main() 
 {
-
+	//s2h starts in the bss section because it is uninitialized
+	int i = func_one(j);
+}
+func_one(char * j)//this is for part one bss to stack
+{
+	printf("Enter a 12 letter word:\n");
+	scanf_s("%s", j);//this will be on the stack
+	return 0;
 }
